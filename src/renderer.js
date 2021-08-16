521a7edcaa2a9244ab399897575eae59f2ed05e4
Dalton Thompson <DaltonThompson@users.noreply.github.com>
2021-08-16 19:16:49 -0400

    the first commit

---
+++ src/renderer.js
@@ -0,0 +1,212 @@
+// dependencies
+const chokidar = require('chokidar'), fs = require('fs'), path = require('path'), os = require('os'), fetch = require('node-fetch');
+
+// External paths
+let server_log = os.homedir + "/Library/Application Support/Steam/SteamApps/common/dota 2 beta/game/dota/server_log.txt";
+// Alt declaration if testing outside of live game
+// let server_log = "./server_log.txt"
+
+// DOM references
+const optionsContainer = document.querySelector('.options_grid');
+
+// Creates object to hold all hero and player data.
+let globalData = {};
+
+// Declares number of players to evaluate. Only change if testing.
+let playersTotalInGame = 10;
+
+function initializeHeroCards(){
+    for (let i = 0; i < globalData.heroStats.length; i++) {
+        for (let j = 0; j < 10; j++) {
+            // Set up element to insert.
+            let heroCard = document.createElement("div"), imageInsert = document.createAttribute("style");
+            heroCard.className = `heroCard grid_column${j}`;
+            imageInsert.value = i < globalData.heroStats.length - 1 ? `background-image: url(https://steamcdn-a.akamaihd.net${globalData.heroStats[i].img})` : `background-image: url(https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/dawnbreaker.png)`;
+            heroCard.setAttributeNode(imageInsert);
+            optionsContainer.appendChild(heroCard);
+            heroCard.classList.add(`hero` + globalData.heroStats[i].id);
+        }
+    }
+}
+
+// Observes log for all events
+chokidar.watch(server_log).on('all', (event, path) => {
+    console.log(event, path, readLog());
+});
+
+// function from dotabuddy. Finds last line of server_log, calls parselog on it.
+function readLog(){
+    let lines = [];
+    fs.readFileSync(server_log).toString().split("\n").forEach(line => lines.push(line));
+    return this.parseLog(lines[lines.length - 2], lines[lines.length - 2], lines[lines.length - 3]);
+};
+
+let steamIds;
+
+// function from dotabuddy. Parses log into steamIds, additional data includes date, time, game mode.
+function parseLog(lastLine, penultLine, secondLastLine) {
+
+    let regex = /(.*?) - (.*?): (.*?) \(Lobby (\d+) (\w+) (.*?)\)/, match, lastLineMatch = lastLine.match(regex), penultLineMatch = penultLine.match(regex), secondLastLineMatch = secondLastLine.match(regex);
+
+    if (lastLineMatch) {
+        console.log('Ongoing game is being processed.');
+        match = lastLineMatch;
+    } else if (penultLineMatch) {
+        console.log('Previous game is being processed.');
+        match = penultLineMatch;
+    } else if (secondLastLineMatch) {
+        console.log('Previous game is being processed.');
+        match = secondLastLineMatch;
+    } else {
+        return console.log('No match found in last line of file.');
+    }
+
+    console.log(`Match: ${match}`);
+
+    // I believe that dateRegex and dateSplit are the same thing?
+    let date = match[1], dateRegex = /\//, time = match[2], dateSplit = date.split("/"), timeSplit = time.split(":"), matchDatetime = new Date(dateSplit[2], dateSplit[0] - 1, dateSplit[1], timeSplit[0], timeSplit[1], timeSplit[2]), server = match[3], lobbyId = match[4], gameMode = match[5], playersString = match[6], playersRegex = /\d:(\[U:\d:\d+])/g, playersMatch;
+    steamIds = [];
+    while (playersMatch = playersRegex.exec(playersString)) {
+        let sid = playersMatch[1].substring(5, playersMatch[1].length - 1);
+        steamIds.push(sid);
+    }
+
+    // Set values for the match on globalData.
+    globalData.match = {
+        'time': matchDatetime,
+        'mode': gameMode,
+        'server': server
+    };
+    globalData.player = [];
+
+    // Affixes steamId to each player, calls promiseChain.
+    for (i = 0; i < playersTotalInGame; i++) globalData.player[i] = {'steamId': steamIds[i]};
+    return promiseChain();
+};
+
+const requestHeroStats = async () => {
+    try {
+        const response = await fetch(`https://api.opendota.com/api/heroStats`)
+        globalData.heroStats = await response.json()
+        console.log(globalData.heroStats);
+    } catch (error) {
+        console.log(error.response);
+    }
+};
+
+const promiseChain = async () => {
+    const a = await requestHeroStats();
+    const e = await initializeHeroCards();
+    const b = await iterateThroughPlayers(requestPlayerPersonal);
+    const c = await iterateThroughPlayers(requestPlayerData);
+    const d = await orderCards();
+    const g = await iterateThroughPlayers(createPlayerCards);
+    return [a, e, b, d, c, g];
+};
+
+const requestPlayerData = async(i) => {
+    try {
+        const response = await fetch(`https://api.stratz.com/api/v1/Player/${steamIds[i]}/heroPerformance?gameVersionId=141&gameVersionId=144`)
+        globalData.player[i].performance = await response.json();
+        globalData.player[i].access = true;
+        console.log(globalData.player[i].performance);
+    } catch (error) {
+        globalData.player[i].access = false;
+        console.log(error);
+    }
+};
+
+const iterateThroughPlayers = async(callback) => {
+    let promiseArray = [];
+    for (let i = 0; i < playersTotalInGame; i++) promiseArray.push(callback(i));
+    return Promise.all(promiseArray);
+};
+
+const requestPlayerPersonal = async(i) => {
+    try {
+        const response = await fetch(`https://api.stratz.com/api/v1/Player/${steamIds[i]}`)
+        globalData.player[i].stratzAccess = true;
+        globalData.player[i].personal = await response.json();
+        console.log(globalData.player[i].personal);
+    } catch (error) {
+        globalData.player[i].stratzAccess = false;
+        console.log(error.response);
+    }
+}
+
+const createPlayerCards = (i) => {
+    let playeri = document.getElementById(`player${i}`);
+    if (globalData.player[i].stratzAccess) {
+        if (globalData.player[i].personal.hasOwnProperty('steamAccount')) {
+            document.querySelector(`#pick${i}main .player_name`).innerText = globalData.player[i].personal.steamAccount.name;
+        } else {
+            console.log(i + ' could not receive avatar or persona name')
+        }
+
+        if (globalData.player[i].personal.steamAccount.seasonRank !== undefined) {
+            let tier = globalData.player[i].personal.steamAccount.seasonRank;
+            let star = Math.floor((tier / 1) % 10), medal = Math.round(tier / 10);
+            if (medal === 1) {
+                document.querySelector(`#pick${i}main .player_rank`).innerText = `Herald ${star}`;
+            } else if (medal === 2) {
+                document.querySelector(`#pick${i}main .player_rank`).innerText = `Guardian ${star}`;
+            } else if (medal === 3) {
+                document.querySelector(`#pick${i}main .player_rank`).innerText = `Crusader ${star}`;
+            } else if (medal === 4) {
+                document.querySelector(`#pick${i}main .player_rank`).innerText = `Archon ${star}`;
+            } else if (medal === 5) {
+                document.querySelector(`#pick${i}main .player_rank`).innerText = `Legend ${star}`;
+            } else if (medal === 6) {
+                document.querySelector(`#pick${i}main .player_rank`).innerText = `Ancient ${star}`;
+            } else if (medal === 7) {
+                document.querySelector(`#pick${i}main .player_rank`).innerText = `Divine ${star}`;
+            } else if (medal === 8) {
+                document.querySelector(`#pick${i}main .player_rank`).innerText = `Immortal ${star}`;
+            } else {
+                document.querySelector(`#pick${i}main .player_rank`).innerText = `UNKNOWN`;
+            }
+        } else {
+            document.querySelector(`#pick${i}main .player_rank`).innerText = `UNKNOWN`;
+        }
+    }
+}
+
+function orderCards() {
+    console.log('Starting orderCards')
+    // Callback for each globalData.heroStats.id
+    globalData.heroStats.forEach(hero => addPlayerProb(hero,hero.id));
+}
+
+function addPlayerProb(hero,idOfHero){
+    hero.winRate = hero[`5_win`]/hero[`5_pick`];
+    // Create obj where array of objects will be saved
+    hero.playerWeights = [];
+    // Collect References to available hero data for players
+    for (let i = 0; i < playersTotalInGame; i++) {
+        if (globalData.player[i].access) {
+            getWinAttempt(hero, i, globalData.player[i].performance.find(playerAsHero => playerAsHero.heroId === idOfHero));
+        } else {
+            let obj = {};
+            obj[`weightedScore`] = hero.winRate;
+            hero.playerWeights[i] = obj;
+        }
+        document.querySelector(`div#options > div.heroCard.hero${idOfHero}.grid_column${i}`).style.order = Math.round(hero.playerWeights[i].weightedScore*-100);
+    }
+};
+
+function getWinAttempt(hero,i,playerAsHero) {
+    // Array to save information
+    let obj = {};
+    // Takes elem as arg. If no data, returns false; else returns info found at index.
+    if (playerAsHero === undefined) {
+        // Removes an arbitrary percent for a hero where the player has no games, because less experience means less ability to perform. Lower winrate heroes are less effected by this. Removes 2.5% from a 50% hero.
+        obj[`weightedScore`] = hero.winRate * 0.95;
+        hero.playerWeights[i] = obj;
+    } else {
+        let convertedIMP = (playerAsHero.imp + 50)/100;
+        obj[`winRate`] = playerAsHero.winCount / playerAsHero.matchCount;
+        // Need 4 games on a hero to be at equal weight with meta.
+        obj[`weightedScore`] = ( ( playerAsHero.matchCount * convertedIMP ) + playerAsHero.winCount + (8 * hero.winRate)) / ( (2 * playerAsHero.matchCount) + 8);
+        hero.playerWeights[i] = obj;
+    }
+}
