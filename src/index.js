79717b7704a21de4110e3f388cac7db0454950f0
Dalton Thompson <DaltonThompson@users.noreply.github.com>
2021-08-17 18:05:13 -0400

    Frameless and menu formatting
    Formatting to frameless window, introduce custom menus including prompt for file path to server_log.txt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, dialog } = require('electron');
 const path = require('path');
+const isMac = process.platform === 'darwin'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -9,8 +10,9 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
+    titleBarStyle: 'hidden',
     width: 1440,
-    height: 850,
+    height: 877,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
@@ -27,7 +29,133 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow();
+
+  const template = [
+    // { role: 'appMenu' }
+    ...(isMac ? [{
+      label: app.name,
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { label: 'Preferences',
+          submenu: [
+            {
+              label: 'Server Log Filepath',
+              click: async () => {
+                dialog.showOpenDialog({
+                  properties: ['openFile'],
+                  defaultPath: '/Library/Application Support/Steam/SteamApps/common/dota 2 beta/game/dota/server_log.txt',
+                  filters: [{ name: 'Valid Files', extensions: ['txt'] }]
+                })
+              }
+            },
+            {
+              label: 'Gameplay Patch Range',
+              click: async () => {}
+            },
+            {
+              label: 'Clear Memory',
+              click: async () => {}
+            }
+          ]
+        },
+        { type: 'separator' },
+        // { role: 'services' },
+        // { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        isMac ? { role: 'close' } : { role: 'quit' },
+        { role: 'quit' }
+      ]
+    }] : []),
+    // // { role: 'fileMenu' }
+    // {
+    //   label: 'File',
+    //   submenu: [
+    //     isMac ? { role: 'close' } : { role: 'quit' }
+    //   ]
+    // },
+    // { role: 'editMenu' }
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...(isMac ? [
+          // { role: 'pasteAndMatchStyle' },
+          { role: 'delete' },
+          { role: 'selectAll' },
+          { type: 'separator' }
+          // {
+          //   label: 'Speech',
+          //   submenu: [
+          //     { role: 'startSpeaking' },
+          //     { role: 'stopSpeaking' }
+          //   ]
+          // }
+        ] : [
+          { role: 'delete' },
+          { type: 'separator' },
+          { role: 'selectAll' }
+        ])
+      ]
+    },
+    // { role: 'viewMenu' }
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        // { role: 'resetZoom' },
+        // { role: 'zoomIn' },
+        // { role: 'zoomOut' },
+        // { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    // { role: 'windowMenu' }
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...(isMac ? [
+          { type: 'separator' },
+          { role: 'front' },
+          { type: 'separator' },
+          { role: 'window' }
+        ] : [
+          { role: 'close' }
+        ])
+      ]
+    }
+    // {
+    //   role: 'help',
+    //   submenu: [
+    //     {
+    //       label: 'Learn More',
+    //       click: async () => {
+    //         const { shell } = require('electron')
+    //         await shell.openExternal('https://electronjs.org')
+    //       }
+    //     }
+    //   ]
+    // }
+  ]
+  
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
