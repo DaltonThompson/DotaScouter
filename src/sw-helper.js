f553686565807e7b2b29e968c4ec557edfd3cf6d
Dalton Thompson <DaltonThompson@users.noreply.github.com>
2021-09-06 17:36:05 -0400

    Separate swHelper into own file

---
+++ src/sw-helper.js
@@ -0,0 +1,16 @@
+const swHelper = {
+    SW: null,
+    init() {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('sw.js').then(registration => {
+                swHelper.SW = registration.installing || registration.waiting || registration.active;
+                console.log('%cSW: %cregistration promise resolved', 'color:#0dd', 'color:#eee')
+            })
+            if (navigator.serviceWorker.controller) console.log('%cSW: %cexists', 'color:#0ff', 'color:#eee')
+        } else {
+            console.log('Service workers unsupported?! In Electron?!')
+        }
+    }
+};
+
+document.addEventListener('DOMContentLoaded', swHelper.init);
