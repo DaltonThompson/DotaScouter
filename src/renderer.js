4c5a60ac5151d3a9210654df252231b0243c1bc8
Dalton Thompson <DaltonThompson@users.noreply.github.com>
2021-09-06 17:36:05 -0400

    Separate swHelper into own file

--- src/renderer.js
+++ src/renderer.js
@@ -9,23 +9,6 @@ let server_log = os.homedir + "/Library/Application Support/Steam/SteamApps/comm
 // DOM references
 const optionsContainer = document.querySelector('.options_grid');
 
-const swHelper = {
-    SW: null,
-    init() {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('sw.js').then(registration => {
-                swHelper.SW = registration.installing || registration.waiting || registration.active;
-                console.log('%cSW: %cregistration promise resolved', 'color:#0dd', 'color:#eee')
-            })
-            if (navigator.serviceWorker.controller) console.log('%cSW: %cexists', 'color:#0ff', 'color:#eee')
-        } else {
-            console.log('Service workers unsupported?! In Electron?!')
-        }
-    }
-};
-
-document.addEventListener('DOMContentLoaded', swHelper.init);
-
 // Creates object to hold all hero and player data.
 let globalData = {
     'gameVersion':[
